1a019e455786ea05a27bec943c8ddf7fafcca3c4
vladwebdev1402 <vlad.kriv.2004@mail.ru>
2024-09-10 01:29:25 +0700

    add adding equation and geting list equations in dbprovider

--- lib/src/services/db.dart
+++ lib/src/services/db.dart
@@ -2,7 +2,8 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
-const int db_version = 1;
+const int dbVersion = 1;
+const String tableName = "Equation";
 
 class DBProvier {
   DBProvier._();
@@ -23,17 +24,31 @@ class DBProvier {
   Future<Database> _initDb() async {
     Directory dir = await getApplicationDocumentsDirectory();
     String path = "${dir.path}coef.db";
-    return await openDatabase(path, version: db_version, onCreate: _createDB);
+    return await openDatabase(path, version: dbVersion, onCreate: _createDB);
   }
 
   Future<void> _createDB(Database db, int version) async {
     await db.execute("""
       CREATE TABLE Equation(
       id INTEGER PRIMARY KEY AUTOINCREMENT,
-      coefA FLOAT,
-      coefB FLOAT,
-      coefC FLOAT,
-    )
-                """);
+      coefA FLOAT NOT NULL,
+      coefB FLOAT NOT NULL,
+      coefC FLOAT NOT NULL,
+      d FLOAT NOT NULL,
+      x1 FLOAT NULL,
+      x2 FLOAT NULL
+    );
+    """);
+  }
+
+  Future<void> addEquation(Map<String, dynamic> equation) async {
+    Database db = await database;
+    await db.insert(tableName, equation);
+  }
+
+  Future<List<Map<String, dynamic>>> getEquations() async {
+    Database db = await database;
+    final List<Map<String, dynamic>> data = await db.query(tableName);
+    return data;
   }
 }
