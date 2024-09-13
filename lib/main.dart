22d55c1954d5ead097dd2a7bbe133dd2f4f35714
vladwebdev1402 <vlad.kriv.2004@mail.ru>
2024-09-13 14:34:58 +0700

    change padding

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,19 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
       title: "Laba 1",
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Лабораторная работа 1"),
-          backgroundColor: Colors.purpleAccent,
-        ),
-        body: Container(
-          margin: EdgeInsets.only(top: 32.0),
-          color: Colors.pink,
-          child: const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text(
-              "Hello World",
-              style: TextStyle(color: Colors.white, fontSize: 32.0),
-            ),
+          appBar: AppBar(
+            title: const Text("Кривенко В.В."),
+            backgroundColor: Colors.purpleAccent,
           ),
-        ),
-      ),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              color: Colors.pink,
+              child: const Text(
+                "Разработка мобильных приложений",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white, fontSize: 32.0),
+              ),
+            ),
+          )),
     ));
