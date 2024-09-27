7744f6dae320ba6b75ccfc129c22b944a79e29e4
vladwebdev1402 <vlad.kriv.2004@mail.ru>
2024-09-27 13:55:53 +0700

    add zero check value

--- lib/src/ui/coefficient_text_field.dart
+++ lib/src/ui/coefficient_text_field.dart
@@ -2,14 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class CoefficientTextField extends StatelessWidget {
-  final String _coeff;
+  final String coeff;
   final TextEditingController controller;
   final FocusNode focusNode;
   final Function onSubmitted;
+  final bool? isNotZero;
 
   const CoefficientTextField(
-      this._coeff, this.controller, this.focusNode, this.onSubmitted,
-      {super.key});
+      {required this.coeff,
+      required this.controller,
+      required this.focusNode,
+      required this.onSubmitted,
+      this.isNotZero,
+      super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +23,8 @@ class CoefficientTextField extends StatelessWidget {
       decoration: InputDecoration(
           errorStyle: const TextStyle(fontSize: 16.0),
           labelStyle: const TextStyle(fontSize: 16.0),
-          labelText: _coeff),
+          errorMaxLines: 2,
+          labelText: coeff),
       keyboardType: TextInputType.number,
       controller: controller,
       focusNode: focusNode,
@@ -29,6 +35,9 @@ class CoefficientTextField extends StatelessWidget {
         if (!RegExp(r'((^|, )([-]{0,1}[0-9]+[.]{0,1}[0-9]+|[-]{0,1}[0-9]+))+$')
             .hasMatch(value)) {
           return "Число введено неккоретно";
+        }
+        if (isNotZero != null && isNotZero! && value == '0') {
+          return "Данный коэффициент не может быть равен 0";
         }
         return null;
       },
